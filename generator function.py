1474b64ca390cb757f509ccff154f560e42efda5
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2026-02-17 21:28:41 +0200

    Delete generator function.py

--- generator function.py
+++
@@ -1,8 +0,0 @@
-def skaiciuok_iki(max_reiksme):
-    skaicius = 0
-    while skaicius < max_reiksme:
-        yield skaicius
-        skaicius += 1
-
-for numeris in skaiciuok_iki(5):
-    print(numeris)
