4c9ad9622acde37c2a15b6da00fd0bb225cb1c8b
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2026-02-19 21:35:30 +0200

    Add files via upload

---
+++ introduction to Python/task 2.py
@@ -0,0 +1,13 @@
+name = "Laimonas"
+print("name")
+print(type("name"))
+
+age = "27"
+print("age")
+print(type("age"))
+
+height = "181"
+print("height")
+print(type("height"))
+
+
---
+++ introduction to Python/task 3.py
@@ -0,0 +1,11 @@
+qualification = "back-end web developer"
+print("back-end web developer")
+print(type("back-end web developer"))
+
+language = "Python"
+print("Python")
+print(type("Python"))
+
+experience = "3 months"
+print("3 months")
+print(type("3 months"))
