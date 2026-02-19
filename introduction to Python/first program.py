52c7108b028056c76891e97f41cb2820b42b876c
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2026-02-19 21:36:28 +0200

    Delete introduction to Python directory

--- introduction to Python/first program.py
+++
@@ -1,4 +0,0 @@
-program = "Hello, World"
-print("Hello, World")
-print(type("Hello, World"))
-
--- introduction to Python/operators.py
+++
@@ -1,8 +0,0 @@
-Operator Name Example
-+	Addition	x + y
--	Substraction	x - y
-*	Multiplication	x * y
-/	Division	x / y
-%	Module x % y
-**	Eksponention	x ** y
-//	Divide from bottom	x // y
